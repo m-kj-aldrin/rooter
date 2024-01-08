a3914f94a383c8158ebcb78b4b00a9d4b7eabf5c
MKJ <mkj.aldrin@icloud.com>
2024-01-08 19:09:45 +0100

    await scripts

--- test/index.js
+++ test/index.js
@@ -1,1 +1,11 @@
 import "../src/rooter.js";
+
+// let script = document.createElement("script");
+// script.textContent = `console.log("GOOD")`;
+
+// document.head.append(script)
+
+// setTimeout(() => {
+//     script.remove()
+//     document.head.append(script);
+// }, 500);
