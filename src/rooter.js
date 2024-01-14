a77bfd151f37101ec7649e2274f157bdbad8ed65
MKJ <mkj.aldrin@icloud.com>
2024-01-14 22:40:54 +0100

    handle head

--- src/rooter.js
+++ src/rooter.js
@@ -4,252 +4,253 @@ const PERSIST_ATTR = "data-rooter-persist";
 const ANIMATE_ATTR = "data-rooter-animate";
 
 /**
- * @template {"fetching" | "view-start" | "view-end"} T
+ * @template {"fetching" | "fetched" | "view-start" | "view-end"} T
  */
 export class RouteEvent extends Event {
-  /**@type {Promise} */
-  #p;
+    /**@type {Promise[]} */
+    #promise = [];
 
-  /**
-   * @param {T} type
-   */
-  constructor(type) {
-    super(`route-${type}`);
-  }
+    /**@param {T} type */
+    constructor(type) {
+        super(`route-${type}`);
+    }
 
-  get p() {
-    return this.#p;
-  }
+    get promise() {
+        return Promise.all(this.#promise);
+    }
 
-  set p(promise) {
-    this.#p = promise;
-  }
-
-  wait(ms = 0) {
-    this.#p = new Promise((res) => {
-      setTimeout(() => {
-        res();
-      }, ms);
-    });
-  }
+    /**@param {Promise} promise */
+    set promise(promise) {
+        this.#promise.push(promise);
+    }
 }
 
 /**@param {HTMLElement} element */
 function is_anchor(element) {
-  if (element instanceof HTMLAnchorElement) {
-    return element;
-  }
+    if (element instanceof HTMLAnchorElement) {
+        return element;
+    }
 }
 
 /**@param {Event} event */
 function get_anchor(event) {
-  for (const element of event.composedPath()) {
-    let anchor = is_anchor(/**@type{HTMLElement}*/ (element));
-    if (anchor) {
-      return anchor;
+    for (const element of event.composedPath()) {
+        let anchor = is_anchor(/**@type{HTMLElement}*/ (element));
+        if (anchor) {
+            return anchor;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
-// TODO - Do we need to create a new script element to make the browser execute it when appended if its allready is a part of a document?
+const SCRIPT_EXC_ATTR = "data-rooter-execute";
 
 /**@param {Document} new_document */
-function attach_incoming_scripts(new_document) {
-  let current_head = document.head;
+function mark_incoming_scripts(new_document) {
+    /**@type {NodeListOf<HTMLScriptElement>} */
+    let scripts = new_document.querySelectorAll("script[src]");
 
-  // /**@type {NodeListOf<HTMLScriptElement>} */
-  // let current_scripts = current_head.querySelectorAll("script[src]");
-  let current_scripts = document.scripts;
-
-  // /**@type {NodeListOf<HTMLScriptElement>} */
-  // let new_scripts = new_head.querySelectorAll("script[src]");
-  let incoming_scripts = new_document.scripts;
-
-  /**@type {HTMLScriptElement[]} */
-  let new_scripts = [];
-
-  // for (const incoming of incoming_scripts) {
-  //   for (const current of current_scripts) {
-  //     if (incoming.src != current.src || !incoming.src) {
-  //       let new_script_el = document.createElement("script");
-  //       new_script_el.src = incoming.src;
-  //       new_script_el.innerHTML = incoming.innerHTML;
-
-  //       new_scripts.push(new_script_el);
-  //     }
-  //   }
-  // }
-
-  for (const incoming of incoming_scripts) {
-    for (const current of current_scripts) {
-      if (incoming.src == current.src) {
-        incoming.remove();
-      }
+    for (const script of scripts) {
+        let src = script.getAttribute("src");
+        let match = document.head.querySelector(`script[src='${src}']`);
+        if (!match) {
+            script.toggleAttribute(SCRIPT_EXC_ATTR);
+        }
     }
-  }
 
-  // let bare_new_scripts = [
-  //   ...new_document.querySelectorAll("script:not([src])"),
-  // ].map((script) => {
-  //   let fresh_script = document.createElement("script");
-  //   fresh_script.textContent = script.textContent;
-  //   return fresh_script;
-  // });
-
-  // /**@type {HTMLScriptElement[]} */
-  // let new_scripts_dif = bare_new_scripts;
-
-  // for (let i = 0; i < new_scripts.length; i++) {
-  //   const new_script = new_scripts[i];
-
-  //   /**@type {HTMLScriptElement} */
-  //   let match;
-  //   for (let j = 0; j < current_scripts.length; j++) {
-  //     const current_script = current_scripts[j];
-  //     if (new_script.src == current_script.src) {
-  //       match = new_script;
-  //     }
-  //   }
-
-  //   if (!match) {
-  //     let fresh_script = document.createElement("script");
-  //     fresh_script.src = new_script.src;
-  //     new_scripts_dif.push(fresh_script);
-  //   }
-  // }
-
-  // return new_scripts_dif;
-  return [];
+    for (const script of new_document.querySelectorAll("script:not([src])")) {
+        script.toggleAttribute(SCRIPT_EXC_ATTR);
+    }
 }
 
-/**@param {HTMLHeadElement} new_head */
-function dif_styles(new_head) {
-  let new_styles = new_head.querySelectorAll("style");
+/**@param {HTMLScriptElement} source */
+function build_fresh_script(source) {
+    let script = document.createElement("script");
+    script.innerHTML = source.innerHTML;
+    for (const attr of source.attributes) {
+        script.setAttribute(attr.name, attr.value);
+    }
+    script.removeAttribute(SCRIPT_EXC_ATTR);
+    return script;
+}
 
-  return new_styles;
+function run_marked_scripts() {
+    /**@type {Promise[]} */
+    let script_promise = [];
+    for (const script of [...document.scripts]) {
+        if (!script.hasAttribute(SCRIPT_EXC_ATTR)) continue;
+        let fresh_script = build_fresh_script(script);
+        script.replaceWith(fresh_script);
+
+        script_promise.push(new Promise((res) => (fresh_script.onload = res)));
+    }
+
+    return script_promise;
 }
 
 /**@param {Document} new_document */
-function dif_head(new_document) {
-  let new_head = new_document.head;
+function preload_styles(new_document) {
+    let style_links = new_document.head.querySelectorAll(
+        "link[rel='stylesheet']"
+    );
 
-  let new_scripts = attach_incoming_scripts(new_document);
-  let new_styles = dif_styles(new_head);
+    /**@type {Promise[]} */
+    let link_promises = [];
 
-  return {
-    scripts: new_scripts,
-    styles: new_styles,
-  };
+    for (const link of style_links) {
+        const href = link.getAttribute("href");
+        let match = document.head.querySelector(`link[href='${href}']`);
+        if (!match) {
+            let preload_link = document.createElement("link");
+            preload_link.rel = "preload";
+            preload_link.href = href;
+            preload_link.as = "style";
+
+            document.head.appendChild(preload_link);
+
+            link_promises.push(
+                new Promise(
+                    (res) =>
+                        (preload_link.onload = () => {
+                            preload_link.remove();
+                            res();
+                        })
+                )
+            );
+        } else {
+            //...
+        }
+    }
+
+    return link_promises;
+}
+
+/**@param {Document} new_document */
+function update_head(new_document) {
+    for (const element of [...document.head.children]) {
+        if (element.matches("link[rel=stylesheet]")) {
+            const href = element.getAttribute("href");
+            const exist_element = new_document.head.querySelector(
+                `link[rel=stylesheet][href='${href}']`
+            );
+
+            // If element already in document remove incoming element
+            // Else, it doesnt exist in the incoming document, remove it from head
+            if (exist_element) {
+                exist_element.remove();
+            } else {
+                element.remove();
+            }
+        } else if (element.matches("script[src]")) {
+            let src = element.getAttribute("src");
+            const exist_element = new_document.head.querySelector(
+                `script[src='${src}']`
+            );
+
+            if (exist_element) {
+                exist_element.remove();
+            } else {
+                element.remove();
+            }
+        } else if (!element.matches("link[rel=preload]")) {
+            element.remove();
+        }
+    }
 }
 
 async function fetch_document(path) {
-  let res = await fetch(path);
-  let text = await res.text();
-  let new_document = new DOMParser().parseFromString(text, "text/html");
-  return new_document;
-}
-
-/**@param {HTMLScriptElement[]} scripts */
-async function await_script_load(scripts) {
-  return Promise.all(
-    scripts
-      .filter((script) => script.src)
-      .map((script) => {
-        return new Promise((res) => {
-          script.onload = () => res();
-        });
-      })
-  );
+    let res = await fetch(path);
+    let text = await res.text();
+    let new_document = new DOMParser().parseFromString(text, "text/html");
+    return new_document;
 }
 
 /**@param {Document} doc */
 function get_persist_elements(doc) {
-  return doc.querySelectorAll(`[${PERSIST_ATTR}]`);
+    return doc.querySelectorAll(`[${PERSIST_ATTR}]`);
 }
 
 async function route(pathname) {
-  window.dispatchEvent(new RouteEvent("fetching"));
+    window.dispatchEvent(new RouteEvent("fetching"));
 
-  let new_document = await fetch_document(pathname);
+    let new_document = await fetch_document(pathname);
 
-  let new_assets = dif_head(new_document);
+    window.dispatchEvent(new RouteEvent("fetched"));
 
-  document.head.querySelectorAll("script:not([src])").forEach((script) => {
-    script.remove();
-  });
+    // TODO - preload new scripts aswell
+    mark_incoming_scripts(new_document);
+    let style_promises = preload_styles(new_document);
 
-  let scripts_loaded = await_script_load(new_assets.scripts);
-  document.head.append(...new_assets.scripts);
+    let start_event = new RouteEvent("view-start");
+    window.dispatchEvent(start_event);
 
-  await scripts_loaded;
+    await start_event.promise;
 
-  let start_event = new RouteEvent("view-start");
-  window.dispatchEvent(start_event);
+    // document.head.replaceWith(new_document.head);
+    update_head(new_document);
+    document.head.append(...new_document.head.children);
 
-  await start_event.p;
+    run_marked_scripts();
 
-  document.head.querySelectorAll("style").forEach((style) => style.remove());
-  document.head.append(...new_assets.styles);
+    let old_body = document.body;
 
-  let old_body = document.body;
+    // Check new body and look for persist elements, find coresponding element in the old body and use it if exist
+    get_persist_elements(new_document).forEach((el) => {
+        let id = el.getAttribute(PERSIST_ATTR);
+        let old_el = old_body.querySelector(`[${PERSIST_ATTR}=${id}]`);
+        if (old_el) {
+            el.replaceWith(old_el);
+        } else {
+            //...
+        }
+    });
 
-  // Check new body and look for persist elements, find coresponding element in the old body and use it if exist
-  get_persist_elements(new_document).forEach((el) => {
-    let id = el.getAttribute(PERSIST_ATTR);
-    let old_el = old_body.querySelector(`[${PERSIST_ATTR}=${id}]`);
-    if (old_el) {
-      el.replaceWith(old_el);
-    } else {
-      //...
-    }
-  });
+    await Promise.all(style_promises);
+    document.body.replaceWith(new_document.body);
 
-  document.body.replaceWith(new_document.body);
-
-  window.dispatchEvent(new RouteEvent("view-end"));
+    window.dispatchEvent(new RouteEvent("view-end"));
 }
 
 document.addEventListener("click", (e) => {
-  e.preventDefault();
-  let anchor = get_anchor(e);
-  if (anchor) {
-    e.preventDefault();
+    let anchor = get_anchor(e);
+    if (anchor) {
+        e.preventDefault();
 
-    if (anchor.pathname == location.pathname) {
-      return;
+        if (anchor.pathname == location.pathname) {
+            return;
+        }
+
+        e.preventDefault();
+        // TODO - build a guard against non trailing slash paths
+        route(anchor.pathname);
+
+        history.pushState(null, null, anchor.pathname);
     }
-
-    e.preventDefault();
-    route(anchor.pathname);
-
-    history.pushState(null, null, anchor.pathname);
-  }
 });
 
 window.addEventListener("popstate", (e) => {
-  route(location.pathname);
+    route(location.pathname);
 });
 
 // + + + ANIMATION - - -
 
 const animation_type_map = {
-  slide: slide_animation,
-  fade: fade_animation,
+    slide: slide_animation,
+    fade: fade_animation,
 };
 
 function default_animation() {
-  let animate = document.body.getAttribute(ANIMATE_ATTR);
-  if (animate != "none") {
-    /**@type {keyof typeof animation_type_map} */
-    let animation_type = document.body.getAttribute(ANIMATE_ATTR) ?? "fade";
-    let animation = animation_type_map[animation_type];
+    let animate = document.body.getAttribute(ANIMATE_ATTR);
+    if (animate != "none") {
+        /**@type {keyof typeof animation_type_map} */
+        let animation_type = document.body.getAttribute(ANIMATE_ATTR) ?? "fade";
+        let animation = animation_type_map[animation_type];
 
-    return [
-      new Promise((res) => (animation.run(document.body).onfinish = res)),
-    ];
-  }
-  return [];
+        return [
+            new Promise((res) => (animation.run(document.body).onfinish = res)),
+        ];
+    }
+    return [];
 }
 
 /**
@@ -257,55 +258,67 @@ function default_animation() {
  * @param {PlaybackDirection} direction
  *  */
 function animate_elements(doc, direction = "normal") {
-  /**@type {NodeListOf<HTMLElement>} */
-  let elements = doc.querySelectorAll(`[${ANIMATE_ATTR}]`);
+    /**@type {NodeListOf<HTMLElement>} */
+    let elements = doc.querySelectorAll(`[${ANIMATE_ATTR}]`);
 
-  let animation_promises = [...elements]
-    .filter((el) => el.getAttribute(ANIMATE_ATTR) != "none")
-    .map((element) => {
-      /**@type {keyof typeof animation_type_map} */
-      let animation_type = element.getAttribute(ANIMATE_ATTR);
+    let animation_promises = [...elements]
+        .filter((el) => el.getAttribute(ANIMATE_ATTR) != "none")
+        .map((element) => {
+            /**@type {keyof typeof animation_type_map} */
+            let animation_type = element.getAttribute(ANIMATE_ATTR);
 
-      if (!(animation_type in animation_type_map)) return Promise.resolve();
+            if (!(animation_type in animation_type_map))
+                return Promise.resolve();
 
-      let animation = animation_type_map[animation_type];
+            let animation = animation_type_map[animation_type];
 
-      return new Promise((res) => {
-        animation.run(element, { direction }).onfinish = res;
-      });
-    });
+            return new Promise((res) => {
+                animation.run(element, { direction }).onfinish = res;
+            });
+        });
 
-  return animation_promises;
+    return animation_promises;
 }
 
 /**@param {RouteEvent<"view-start">} e */
 async function view_start_handler(e) {
-  let animation_promise = default_animation();
-  animation_promise.push(...animate_elements(document));
+    let animation_promise = default_animation();
+    animation_promise.push(...animate_elements(document));
 
-  e.p = Promise.all(animation_promise);
+    e.promise = Promise.all(animation_promise);
 }
 
 /**@param {RouteEvent<"view-end">} e */
 function view_end_handler(e) {
-  /**@type {Promise[]} */
-  let animation_promise = [];
+    let animation_promise = default_animation();
 
-  let animate = document.body.getAttribute(ANIMATE_ATTR);
-  if (animate != "none") {
-    animation_promise.push(
-      new Promise(
-        (res) =>
-          (fade_animation.run(document.body, {
-            direction: "reverse",
-          }).onfinish = res)
-      )
-    );
-  }
-
-  animation_promise.push(...animate_elements(document, "reverse"));
-  e.p = Promise.all(animation_promise);
+    animation_promise.push(...animate_elements(document, "reverse"));
+    e.promise = Promise.all(animation_promise);
 }
+
+/**@type {Animation} */
+let documentElementAnimation;
+
+/**@param {RouteEvent<"fetching">} e */
+function fetching_animation(e) {
+    documentElementAnimation = document.documentElement.animate(
+        [{ opacity: 1 }, { opacity: 1, transform: "translateY(15px)" }],
+        {
+            duration: 3000,
+            fill: "forwards",
+        }
+    );
+}
+
+/**@param {RouteEvent<"fetched">} e */
+function fecthed_animation(e) {
+    documentElementAnimation.pause();
+    documentElementAnimation.updatePlaybackRate(4);
+    documentElementAnimation.reverse();
+}
+
+window.addEventListener("route-fetching", fetching_animation);
+window.addEventListener("route-fetched", fecthed_animation);
 
 window.addEventListener("route-view-start", view_start_handler);
 window.addEventListener("route-view-end", view_end_handler);
